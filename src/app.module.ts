2909ea82ff14109d8e1860373180bc160e39c821
Calvin Adhikang <calvinadhikang@gmail.com>
2024-01-09 11:56:31 +0700

    add database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MenuModule } from './menu/menu.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MenuModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'bkc',
+      entities: [],
+      synchronize: true,
+    }),
+    MenuModule],
   controllers: [AppController],
   providers: [AppService],
 })
