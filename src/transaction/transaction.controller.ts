2ed25d833898b30f3cf998e9406bf64411895382
Calvin Adhikang <calvinadhikang@gmail.com>
2024-03-23 17:57:27 +0700

    update and filter transaction

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,14 +1,21 @@
 import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
+import { time } from 'console';
+import internal from 'stream';
+import { Sesi } from 'src/entities/sesi.entity';
 
 @Controller('transaction')
 export class TransactionController {
     constructor(private readonly transactionService: TransactionService){}
 
     @Get()
-    findAll(){
-        return this.transactionService.findAll();
+    findAll(@Query('time') time: string = 'today', @Query('sesi') sesi: string = 'now'){
+        if (time == undefined || time == "") {
+            return this.transactionService.findAll();
+        }else{
+            return this.transactionService.findByTimeAndSesi(time, sesi)
+        }
     }
 
     @Get(':id')
@@ -19,5 +26,10 @@ export class TransactionController {
     @Post()
     create(@Body() createTransactionDto: CreateTransactionDto){
         return this.transactionService.create(createTransactionDto)
+    }
+
+    @Patch(':id')
+    setLunasById(@Param('id') header_id: number){
+        return this.transactionService.setLunas(header_id)
     }
 }
--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -3,10 +3,9 @@ import { BadRequestException, Injectable, NotFoundException } from '@nestjs/comm
 import { InjectRepository } from '@nestjs/typeorm';
 import { DetailTransaction } from 'src/entities/detail-transaction.entity';
 import { HeaderTransaction } from 'src/entities/header-transaction.entity';
-import { Any, EntityManager, LessThan, MoreThan, Repository, Transaction } from 'typeorm';
+import { Between, LessThan, NumericType, Repository } from 'typeorm';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { User } from 'src/entities/user.entity';
-import { createHash } from 'crypto';
 import { Sesi } from 'src/entities/sesi.entity';
 import { Menu } from 'src/entities/menu.entity';
 
@@ -36,6 +35,57 @@ export class TransactionService {
         return await this.headerRepository.find();
     }
 
+    async findByTimeAndSesi(time:string, sesiId: string){
+        const todayStart = new Date();
+        todayStart.setHours(0, 0, 0, 0); // Set time to 00:01 AM
+
+        const todayEnd = new Date();
+        todayEnd.setHours(23, 59, 59, 999); // Set time to 23:59:59 PM
+
+        let sesi: null | string | number = null;
+        if (sesiId == 'now') {
+            sesi = (await this.sesiService.getSesiNow()).id
+        }else{
+            sesi = parseInt(sesiId)
+        }
+
+        console.log(time)
+        console.log(sesi)
+        
+        if (time == 'today') {
+            if (sesi) {
+                return await this.headerRepository.find({
+                    where: {
+                        sesi: sesi,
+                        createdAt: Between(todayStart, todayEnd) // createdAt is between todayStart and todayEnd
+                    }
+                });
+            }else{
+                return await this.headerRepository.find({
+                    where: {
+                        createdAt: Between(todayStart, todayEnd) // createdAt is between todayStart and todayEnd
+                    }
+                });
+            }
+        }
+        else if(time == 'past') {
+            if (sesi) {
+                return await this.headerRepository.find({
+                    where: {
+                        sesi: sesi,
+                        createdAt: LessThan(todayStart) // createdAt is between todayStart and todayEnd
+                    }
+                });
+            }else{
+                return await this.headerRepository.find({
+                    where: {
+                        createdAt: LessThan(todayStart) // createdAt is between todayStart and todayEnd
+                    }
+                });
+            }
+        }
+    }
+
     async getDetail(header_id: number){
         try {
             const header = await this.headerRepository.findOne({
@@ -47,8 +97,9 @@ export class TransactionService {
 
             const result = { 
                 ...header, 
-                employee: await this.userRepository.findOne({where: {id: header.employee}}), 
+                employee_detail: await this.userRepository.findOne({where: {id: header.employee}}), 
             }
+
             return result
         } catch (error) {
             throw new NotFoundException('Transaction not found');
@@ -71,6 +122,22 @@ export class TransactionService {
         } catch (error) {
             console.log(error)
             return error;
+        }
+    }
+
+    async setLunas(header_id: number){
+        try {
+            await this.headerRepository.findOneByOrFail({id: 33})
+            await this.headerRepository.update(33, { status: "Lunas" })
+            return {
+                error: false,
+                message: "Berhasil Update Status"
+            }
+        } catch (error) {
+            return {
+                error: true,
+                message: "Gagal Update Status, Transaksi Tidak Ditemukan !"
+            }
         }
     }
 }
